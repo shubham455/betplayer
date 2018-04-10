137fb42f1287ce9a62ee69619318bef5ff68635c
shubham455 <shubham1979@hotmail.com>
2018-04-11 00:28:03 +0530

    changes update

--- betplayer/Agent/ClientDetails.aspx.cs
+++ betplayer/Agent/ClientDetails.aspx.cs
@@ -12,93 +12,45 @@ namespace betplayer.Agent
 {
     public partial class ClientDetails : System.Web.UI.Page
     {
+        private DataTable dt;
+        public DataTable MatchesDataTable { get { return dt; } }
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                if(Request.QueryString["msg"] == "Add")
+                if (Request.QueryString["msg"] == "Add")
                 {
                     ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Client Added SuccessFully.....');", true);
                 }
-            }
-        }
-        public string getStudentData()
-        {
-            string data = "";
-            string constr = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
-            {
-                using (SqlCommand cmd = new SqlCommand("Select * From ClientMaster"))
+                string CN = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
+                using (SqlConnection cn = new SqlConnection(CN))
                 {
-                    cmd.Connection = con;
-
-                    cmd.Connection.Open();
-                    using (SqlDataReader sqlRdr = cmd.ExecuteReader())
-                    {
-                        // table = new DataTable();  
-                        // table.Load(reader);  
-                        if (sqlRdr.HasRows)
-                        {
-                            while (sqlRdr.Read())
-                            {
-                                int SNo = sqlRdr.GetInt32(0);
-                                string Name = (sqlRdr.GetString(1));
-                                string ContactNo = sqlRdr.GetString(2);
-                                string Passwod = sqlRdr.GetString(3);
-                                string AgentLimit = sqlRdr.GetString(5);
-                                string ClientLimit = sqlRdr.GetString(4);
-
-                                string AgentShare = sqlRdr.GetString(6);
-                                string ClientShare = sqlRdr.GetString(7);
-                                string Client_Type = sqlRdr.GetString(8);
-                                string Status = sqlRdr.GetString(9);
-                                data += "<tr><td></td><td></td><td>" + SNo + "</td><td></td><td>" + Name + "</td><td>" + ContactNo + "</td><td>" + Passwod + "</td><td></td><td></td><td></td><td>" + Client_Type + "</td><td></td><td></td><td>" + AgentShare + "</td><td>"+ClientShare+"</td><td></td><td>"+Status+"</td></tr>";
-                            }
-                        }
-                    }
+                    cn.Open();
+                    string s = "Select * From ClientMaster";
+                    SqlCommand cmd = new SqlCommand(s, cn);
+                    SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adp.Fill(dt);
                 }
-                return data;
             }
         }
-    
+
+
         protected void txtsearch_TextChanged(object sender, EventArgs e)
         {
-            string data = "";
-            string constr = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
+            string CN = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
+            using (SqlConnection cn = new SqlConnection(CN))
             {
-                using (SqlCommand cmd = new SqlCommand("Select * From ClientMaster Where Name Like '%" + txtsearch.Text + "%'"))
-                {
-                    cmd.Connection = con;
-
-                    cmd.Connection.Open();
-                    using (SqlDataReader sqlRdr = cmd.ExecuteReader())
-                    {
-                        // table = new DataTable();  
-                        // table.Load(reader);  
-                        if (sqlRdr.HasRows)
-                        {
-                            while (sqlRdr.Read())
-                            {
-                                int SNo = sqlRdr.GetInt32(0);
-                                string Name = (sqlRdr.GetString(1));
-                                string ContactNo = sqlRdr.GetString(2);
-                                string Passwod = sqlRdr.GetString(3);
-                                string AgentLimit = sqlRdr.GetString(5);
-                                string ClientLimit = sqlRdr.GetString(4);
-
-                                string AgentShare = sqlRdr.GetString(6);
-                                string ClientShare = sqlRdr.GetString(7);
-                                string Client_Type = sqlRdr.GetString(8);
-                                string Status = sqlRdr.GetString(9);
-                                data += "<tr><td></td><td></td><td>" + SNo + "</td><td></td><td>" + Name + "</td><td>" + ContactNo + "</td><td>" + Passwod + "</td><td></td><td></td><td></td><td>" + Client_Type + "</td><td></td><td></td><td>" + AgentShare + "</td><td>" + ClientShare + "</td><td></td><td>" + Status + "</td></tr>";
-                            }
-                        }
-                    }
-                }
-             
+                cn.Open();
+                string s = "Select* From ClientMaster Where Name Like '%" + txtsearch.Text + "%'";
+                SqlCommand cmd = new SqlCommand(s, cn);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
             }
+
         }
+
         public void BindData()
         {
             string CN = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
@@ -110,7 +62,8 @@ namespace betplayer.Agent
                 SqlDataAdapter adp = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable();
                 adp.Fill(dt);
-               
+
+
             }
         }
     }
